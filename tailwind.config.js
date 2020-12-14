bc63e917905a02d549bf5879ce9596ee72dafe2c
ayodeji334 <fawumiayodeji@gmail.com>
2020-12-14 09:32:17 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    backgroundImage: [ 'responsive', 'hover', 'focus']
+  },
+  plugins: [],
+}
