16f686ec5360d33087afc5749cb42171ee3d02b8
ayodeji334 <fawumiayodeji@gmail.com>
2020-12-14 09:28:28 +0100

    update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -178,6 +178,9 @@ function Dashboard({ countryInfo, statesInfo }) {
                       <h5 className="font-weight-bold">
                         <strong>5 ways to donate to support goverment </strong>
                       </h5>
+                      <svg class="h-6 w-6 text-green-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                      </svg>
                       <Link to="#">
                         Donate Now <ArrowForward fontSize={"small"} />
                       </Link>
